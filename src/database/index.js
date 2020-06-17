4da8e1254cf9aff04ce61f8b19644b1cbf9b40b0
Rubens Guimarães <guimaraes.rubens@gmail.com>
2020-06-16 23:26:06 -0300

    Listando agenda do prestador de serviço e configurando o MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,7 @@
-// Sequelize - responsável pela conexão com o banco
+// Sequelize - responsável pela conexão com o banco PostgresSQL
 import Sequelize from 'sequelize';
+// Mongoose - responsável pela conexão com o banco MongoDB
+import mongoose from 'mongoose';
 
 // Importação dos models
 import User from '../app/models/User';
@@ -15,6 +17,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -27,6 +30,18 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    // Faz a conexão com o banco de dados
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobeautysalon',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
